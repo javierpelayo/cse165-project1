fe8b43274812d2a75148765c51ebd80c8b5c0c0b
Javier Perez Pelayo <contact@javierperez.dev>
2024-04-21 23:19:57 -0700

    update script

--- Assets/Scripts/RenderRay.cs
+++ Assets/Scripts/RenderRay.cs
@@ -6,7 +6,6 @@ public class RenderRay : MonoBehaviour
 {
     public LineRenderer lr;
     public float rayLength = 100.0f;
-    public Transform controllerTransform;
 
     // Start is called before the first frame update
     void Start()
@@ -17,17 +16,19 @@ public class RenderRay : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Ray ray = new Ray(controllerTransform.position, Camera.main.transform.forward);
+        Ray ray = new Ray(new Vector3(0,0,0), new Vector3(0,0,1));
         RaycastHit hit;
-        Vector3 endPosition = controllerTransform.position + Camera.main.transform.forward * rayLength;
+        Vector3 endPosition = new Vector3(0,0,rayLength);
 
-        bool rayHits = Physics.Raycast(ray, out hit, rayLength);
+        bool rayHits = Physics.Raycast(Vector3.zero, new Vector3(0,0,1), out hit, rayLength);
 
         if(rayHits)
         {
-            endPosition = hit.point;
-            Debug.Log("Hit: " + endPosition);
+            endPosition = new Vector3(0.0f, 0.0f, hit.distance);
+            Debug.Log("Hit distance: " + hit.distance);
         }
+
+        // Debug
 
         lr.SetPosition(0, Vector3.zero); // start
         lr.SetPosition(1, endPosition); // end
