7c3b5ce91066c2bb9d9cfb4bdb61100b83c1e81f
Javier Perez Pelayo <contact@javierperez.dev>
2024-04-26 00:58:47 -0700

    update

--- Assets/Scripts/RenderRay.cs
+++ Assets/Scripts/RenderRay.cs
@@ -8,7 +8,8 @@ public class RenderRay : MonoBehaviour
     public float rayLength = 10.0f;
 
     public GameObject itemPrefab;  // Drag your prefab here in the inspector
-    public LayerMask interactableLayers; 
+    public LayerMask interactableLayers;
+    public Camera mainCamera = Camera.main;
 
     // Start is called before the first frame update
     void Start()
@@ -19,15 +20,15 @@ public class RenderRay : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Ray ray = new Ray(new Vector3(0,0,0), new Vector3(0,0,1));
+        Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
-        Vector3 endPosition = new Vector3(0,0,rayLength);
+        Vector3 endPosition = new Vector3(transform.position.x + transform.forward.x * rayLength, transform.position.y + transform.forward.y * rayLength, transform.position.z + transform.forward.z * rayLength);
 
         bool rayHits = Physics.Raycast(transform.position, transform.forward, out hit, rayLength);
 
         if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger, OVRInput.Controller.RTouch))
         {
-            if (rayHits) {
+            if (rayHits)
             {
                 // If the ray hits, spawn the item at the hit point
                 Instantiate(itemPrefab, hit.point, Quaternion.identity);
@@ -39,12 +40,17 @@ public class RenderRay : MonoBehaviour
             }
         }
 
-        if(rayHits)
-        {
-            endPosition = new Vector3(0.0f, 0.0f, hit.distance);
+        Debug.Log("Controller: transform position: " + transform.position + ", transform forward:" + transform.forward);
+        Debug.Log("Main Camera: transform position: " + mainCamera.transform.position + ", transform forward:" + mainCamera.transform.forward);
+
+        if(rayHits) {
+            //endPosition = new Vector3(hit.collider.transform.position.x, hit.collider.transform.position.y, transform.position.z + hit.distance);
+            Debug.Log("Hit!");
+        } else {
+            Debug.Log("Not Hit!");
         }
 
-        lr.SetPosition(0, Vector3.zero); // start
+        lr.SetPosition(0, transform.position); // start
         lr.SetPosition(1, endPosition); // end
     }
 }
